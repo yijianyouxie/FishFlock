73edd584cb4a3b0a4ceb7201ed729eb051008a54
liuhaibin <liuhaibin@cyou-inc.com>
2020-05-21 17:27:17 +0800

    FishFlock工程提交
    Signed-off-by: liuhaibin <liuhaibin@cyou-inc.com>

---
+++ FishFlock/Assets/FishFlock/Assets/Scripts/Utils/CustomTitle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FishFlock
+{
+    public class CustomTitle : PropertyAttribute
+    {
+        public string title;
+        public CustomTitle(string title)
+        {
+            this.title = title;
+        }
+    }
+}
---
+++ FishFlock/Assets/FishFlock/Assets/Scripts/Utils/VecHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FishFlock
+{
+    public class VecHelpers
+    {
+        public static Vector3 mul(Vector3 l, Vector3 r)
+        {
+            Vector3 new_v = l;
+
+            new_v.x *= r.x;
+            new_v.y *= r.y;
+            new_v.z *= r.z;
+
+            return new_v;
+        }
+
+        public static Vector3 mul(Vector3 l, float r)
+        {
+            Vector3 new_v = l;
+
+            new_v.x *= r;
+            new_v.y *= r;
+            new_v.z *= r;
+
+            return new_v;
+        }
+    }
+}
